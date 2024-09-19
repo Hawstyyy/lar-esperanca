8b2bc94c94a293186a5a2bbc29dcf956d1d6977c
Luis Fernando <luisvirissimo789@gmail.com>
2024-09-19 08:20:24 -0400

    Mudado fundo de cinza para branco
    Para refletir o protótipo.

--- login.py
+++ login.py
@@ -19,25 +19,25 @@ root.title("Lar Esperança") # Define o nome da janela, visto no canto esquerdo 
 
 ## Todas as páginas não variáveis que tem no programa
 ### Login
-p_login = ctk.CTkFrame(root, fg_color= 'gray', corner_radius= 0)
+p_login = ctk.CTkFrame(root, fg_color= 'white', corner_radius= 0)
 p_login.place(relheight=1,relwidth=1)
 ### Tela Principal
-p_principal = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
+p_principal = ctk.CTkFrame(root,bg_color='white', corner_radius= 0)
 p_principal.place(relheight=1,relwidth=1)
 ### Tabela Pacientes
-p_pacientes = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
+p_pacientes = ctk.CTkFrame(root,bg_color='white', corner_radius= 0)
 p_pacientes.place(relheight=1,relwidth=1)
 ### Nova Receita
-p_receita = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
+p_receita = ctk.CTkFrame(root,bg_color='white', corner_radius= 0)
 p_receita.place(relheight=1,relwidth=1)
 ### Sinal Vital
-p_sinal_vital = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
+p_sinal_vital = ctk.CTkFrame(root,bg_color='white', corner_radius= 0)
 p_sinal_vital.place(relheight=1,relwidth=1)
 ### Intercorrência
-p_intercorrencia = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
+p_intercorrencia = ctk.CTkFrame(root,bg_color='white', corner_radius= 0)
 p_intercorrencia.place(relheight=1,relwidth=1)
 ### Registro de Funcionário
-p_registro = ctk.CTkFrame(root, fg_color= 'gray', corner_radius= 0)
+p_registro = ctk.CTkFrame(root, fg_color= 'white', corner_radius= 0)
 p_registro.place(relheight=1,relwidth=1)
 
 
