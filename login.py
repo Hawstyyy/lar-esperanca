81b656a2a329e4a539795a8fe0884cce123db4f8
Luis Fernando <luisvirissimo789@gmail.com>
2024-09-19 07:55:40 -0400

    Mudanças solicitadas
    Custom tkinter está sendo usado, junto das mudanças necessárias para seu uso.

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 
-import tkinter as tk
+import customtkinter as ctk
 from tkinter import ttk
 from tkinter import messagebox
 import pymysql.cursors # pip install PyMySQL para adquirir essa blibioteca
@@ -9,7 +9,7 @@ import pymysql.cursors # pip install PyMySQL para adquirir essa blibioteca
 
 # Root
 ## È a base de todas as janelas, para mudar todas ao mesmo tempo. Mude o root.
-root = tk.Tk()
+root = ctk.CTk()
 
 root.geometry("1440x900") # Define o tamanho em pixels da janela
 root.title("Lar Esperança") # Define o nome da janela, visto no canto esquerdo superior.
@@ -19,25 +19,29 @@ root.title("Lar Esperança") # Define o nome da janela, visto no canto esquerdo 
 
 ## Todas as páginas não variáveis que tem no programa
 ### Login
-p_login = tk.Frame(root,bg='gray')
+p_login = ctk.CTkFrame(root, fg_color= 'gray', corner_radius= 0)
 p_login.place(relheight=1,relwidth=1)
 ### Tela Principal
-p_principal = tk.Frame(root,bg='gray')
+p_principal = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
 p_principal.place(relheight=1,relwidth=1)
 ### Tabela Pacientes
-p_pacientes = tk.Frame(root,bg='gray')
+p_pacientes = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
 p_pacientes.place(relheight=1,relwidth=1)
 ### Nova Receita
-p_receita = tk.Frame(root,bg='gray')
+p_receita = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
 p_receita.place(relheight=1,relwidth=1)
 ### Sinal Vital
-p_sinal_vital = tk.Frame(root,bg='gray')
+p_sinal_vital = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
 p_sinal_vital.place(relheight=1,relwidth=1)
 ### Intercorrência
-p_intercorrencia = tk.Frame(root,bg='gray')
+p_intercorrencia = ctk.CTkFrame(root,bg_color='gray', corner_radius= 0)
 p_intercorrencia.place(relheight=1,relwidth=1)
 ### Registro de Funcionário
-p_registro = tk.Frame(root,bg='gray')
+p_registro = ctk.CTkFrame(root, fg_color= 'gray', corner_radius= 0)
 p_registro.place(relheight=1,relwidth=1)
+
+
+# Coloca a Pagina de login como pagina inicial
+p_login.tkraise()
 
 root.mainloop() # Faz com que o programa rode.
