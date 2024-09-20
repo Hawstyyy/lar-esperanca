f450dcfe50c4545763c1593f6ac3acd9a23b1594
LuisCunha05 <luisf.cunha05@gmail.com>
2024-09-20 11:10:43 -0400

    tentando transformar hotbar em classe
    yo

--- hotbar.py
+++ hotbar.py
@@ -27,3 +27,13 @@ logo_label.place(relx=.5, rely=.5, anchor='center')
 
 
 janela.mainloop()
+
+class Hotbar(ctk.CTkFrame):
+    def __init__(self, master: ctk.CTkFrame, width: int = 200, height: int = 98, corner_radius: int | str | None = None, border_width: int | str | None = None, bg_color: str | Image.Tuple[str] = "transparent", fg_color: str | Image.Tuple[str] | None = '#8ED6D0', border_color: str | Image.Tuple[str] | None = None, background_corner_colors: Image.Tuple[str | Image.Tuple[str]] | None = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
+        super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
+
+        self.l_logo = ctk.CTkLabel(self, text='', image=imagem('imagens/logo.png', 98, 98))
+        self.l_logo.place(relx=.5, rely=.5, anchor='center')
+
+        self.place(relx=0.5, rely=0, anchor='n', relwidth=1)
+
--- receita.py
+++ receita.py
@@ -3,6 +3,7 @@ from customtkinter import CTkFrame
 from tkinter import messagebox
 from util import Utils as U
 from PIL import Image
+from hotbar import Hotbar
 
 
 BGC = 'white'
@@ -22,6 +23,8 @@ class Receita(CTkFrame):
     def __init__(self, master: Image.Any, width: int = 200, height: int = 200, corner_radius: int | str | None = None, border_width: int | str | None = None, bg_color: str | Image.Tuple[str] = "transparent", fg_color: str | Image.Tuple[str] | None = None, border_color: str | Image.Tuple[str] | None = None, background_corner_colors: Image.Tuple[str | Image.Tuple[str]] | None = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
         super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
         self.place(relheigh=1, relwidth=1)
+
+        self.p_hotbar = Hotbar(self)
 
         #Frame 
         self.p_holder = CTkFrame(self, width=440, height=600, fg_color=BGC)
