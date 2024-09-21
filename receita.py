c38391ac0d7d26e06665e57b69f975a60cf08b06
LuisCunha05 <luisf.cunha05@gmail.com>
2024-09-21 18:11:45 -0300

    Salvando referência para botão adicionar

--- receita.py
+++ receita.py
@@ -2,7 +2,6 @@ import customtkinter as ctk
 from customtkinter import CTkFrame
 from tkinter import messagebox
 from util import Utils as U
-from PIL import Image
 from hotbar import Hotbar
 
 #-----------------------TELA --------------------------------
@@ -18,25 +17,25 @@ class Receita(CTkFrame):
         self.configure(fg_color='white')
         self.place(relheigh=1, relwidth=1)
 
-        self.p_hotbar = Hotbar(self)
+        self.f_hotbar = Hotbar(self)
 
         #Frame 
-        self.p_holder = CTkFrame(self, width=440, height=600, fg_color=self.BGC)
-        self.p_holder.place(relx=0.5, rely=0.5, anchor='center')
+        self.f_holder = CTkFrame(self, width=440, height=600, fg_color=self.BGC)
+        self.f_holder.place(relx=0.5, rely=0.5, anchor='center')
 
         #titulo
-        ctk.CTkLabel(self.p_holder, text='Nova Receita', fg_color=self.BGC, font=U.f_titulo, text_color=self.TX).place(relx=0.5, rely=0, anchor='n')
-        ctk.CTkLabel(self.p_holder, text='', image=U.imagemCTK('imagens/linha_torcida.png', 344, 36)).place(relx=0.5, rely=0.1, anchor='n')
+        ctk.CTkLabel(self.f_holder, text='Nova Receita', fg_color=self.BGC, font=U.f_titulo, text_color=self.TX).place(relx=0.5, rely=0, anchor='n')
+        ctk.CTkLabel(self.f_holder, text='', image=U.imagemCTK('imagens/linha_torcida.png', 344, 36)).place(relx=0.5, rely=0.1, anchor='n')
 
         #Nome paciente
-        ctk.CTkLabel(self.p_holder, text='Nome do paciente', fg_color=self.BGC, font=U.f_simples, text_color=self.TX, anchor='w').place(relx=0.5, rely=0.25, anchor='s', relwidth=1)
+        ctk.CTkLabel(self.f_holder, text='Nome do paciente', fg_color=self.BGC, font=U.f_simples, text_color=self.TX, anchor='w').place(relx=0.5, rely=0.25, anchor='s', relwidth=1)
 
         def optionmenu_callback(choice):
             print("optionmenu dropdown clicked:", choice)
 
         self.lista_pacientes = ['\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t','Lucas', 'Ana', 'João', 'Maria', 'Pedro', 'Carolina', 'Rafael', 'Juliana', 'Bruno', 'Fernanda']
         self.optionmenu = ctk.CTkOptionMenu(
-            self.p_holder,
+            self.f_holder,
             fg_color=self.COR,
             text_color=self.TX,
             font=U.f_simples,
@@ -52,32 +51,26 @@ class Receita(CTkFrame):
         self.optionmenu.place(relx=0.5, rely=0.25, anchor='n', relwidth=1)
         
         #Remedio
-        ctk.CTkLabel(self.p_holder, text='Remédio', fg_color=self.BGC, font=U.f_simples, text_color=self.TX, anchor='w').place(relx=0.5, rely=0.4, anchor='s', relwidth=1)
-        self.e_remedio = ctk.CTkEntry(self.p_holder, corner_radius=10, fg_color=self.COR, font=U.f_simples,text_color=self.TX,  border_width=0)
+        ctk.CTkLabel(self.f_holder, text='Remédio', fg_color=self.BGC, font=U.f_simples, text_color=self.TX, anchor='w').place(relx=0.5, rely=0.4, anchor='s', relwidth=1)
+        self.e_remedio = ctk.CTkEntry(self.f_holder, corner_radius=10, fg_color=self.COR, font=U.f_simples,text_color=self.TX,  border_width=0)
         self.e_remedio.place(relx=0.5, rely=0.4, anchor='n', relwidth=1)
         
         #Hora inicial
-        ctk.CTkLabel(self.p_holder, text='Horário da Primeira Dose', fg_color=self.BGC, font=('Segoe UI', 20), text_color=self.TX, anchor='w').place(relx=0, rely=0.55, anchor='sw', relwidth=0.54)
-        self.e_hora_inicial = ctk.CTkEntry(self.p_holder, corner_radius=10, fg_color=self.COR, font=U.f_simples,text_color=self.TX,  border_width=0)
+        ctk.CTkLabel(self.f_holder, text='Horário da Primeira Dose', fg_color=self.BGC, font=('Segoe UI', 20), text_color=self.TX, anchor='w').place(relx=0, rely=0.55, anchor='sw', relwidth=0.54)
+        self.e_hora_inicial = ctk.CTkEntry(self.f_holder, corner_radius=10, fg_color=self.COR, font=U.f_simples,text_color=self.TX,  border_width=0)
         self.e_hora_inicial.place(relx=0, rely=0.55, anchor='nw', relwidth=0.54)
 
         #Intervalo
-        ctk.CTkLabel(self.p_holder, text='Intervalo(horas)', fg_color=self.BGC, font=U.f_simples, text_color=self.TX, anchor='w').place(relx=1, rely=0.55, anchor='se', relwidth=0.42)
-        self.e_hora_inicial = ctk.CTkEntry(self.p_holder, corner_radius=10, fg_color=self.COR, font=U.f_simples,text_color=self.TX,  border_width=0)
+        ctk.CTkLabel(self.f_holder, text='Intervalo(horas)', fg_color=self.BGC, font=U.f_simples, text_color=self.TX, anchor='w').place(relx=1, rely=0.55, anchor='se', relwidth=0.42)
+        self.e_hora_inicial = ctk.CTkEntry(self.f_holder, corner_radius=10, fg_color=self.COR, font=U.f_simples,text_color=self.TX,  border_width=0)
         self.e_hora_inicial.place(relx=1, rely=0.55, anchor='ne', relwidth=0.42)
 
-        ctk.CTkButton(self.p_holder, text='ADICIONAR', corner_radius=10, fg_color=self.COR, font=U.f_simples,text_color=self.TX,  border_width=0, command=lambda: print('yeee'), hover_color='#2D5E6C').place(relx=0.5, rely=0.7, anchor='n')
-
-
-
-
-
-
-
+        self.b_adicionar = ctk.CTkButton(self.f_holder, text='ADICIONAR', corner_radius=10, fg_color=self.COR, font=U.f_simples,text_color=self.TX,  border_width=0, command=lambda: print('yeee'), hover_color='#2D5E6C')
+        self.b_adicionar.place(relx=0.5, rely=0.7, anchor='n')
 
 if __name__ == "__main__":
     root = ctk.CTk()
-    root.geometry(f'{root.winfo_width()}x{root.winfo_height()}')
+    root.geometry(f'{root.winfo_screenwidth()}x{root.winfo_screenheight()}')
     root.state('zoomed')
     root.configure(fg_color='white')
     root.title("Nova Receita")
