b02b520b2aa1f09fcc70e4596fc02e51ac0e23c0
LuisCunha05 <luisf.cunha05@gmail.com>
2024-09-21 17:42:02 -0300

    Adicionado método imageCTK e tornado todos os métodos em static
    imagemCTk retorna um ctk.CTkImage.
    Mudado todos os métodos da classe para staticmethod

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import hashlib
+from PIL import Image
+import customtkinter as ctk
 
 class Utils:
     f_titulo = ('Segoe UI', 40)
@@ -10,7 +12,8 @@ class Utils:
     def __init__(self) -> None:
         pass
 
-    def basePath(self, file:str = None) -> str:
+    @staticmethod
+    def basePath(file:str = None) -> str:
         """
         Retorna o caminho para a pasta atual em que o script se encontra. Util para adicionar caminho para imagens e outros.
 
@@ -30,7 +33,8 @@ class Utils:
             return os.path.dirname(os.path.abspath(sys.argv[0])) + f'\\{file}'
         return os.path.dirname(os.path.abspath(sys.argv[0]))
 
-    def toSHA256(self, string: str) -> str:
+    @staticmethod
+    def toSHA256(string: str) -> str:
         """
         Retorna a Hash da string de entrada, utilizando algoritmo SHA256
 
@@ -41,3 +45,13 @@ class Utils:
             Uma string contendo a Hash, como: '13ee3badae639da216c9eaa9b104047a1421a40b4e449ea78cc524631a6be75d'.
         """
         return hashlib.sha256(string.encode()).hexdigest()
+
+    @staticmethod
+    def imagemCTK(path: str, largura: int, altura: int):
+        """Retorna uma instância de CTkImage"""
+        img = Image.open(Utils.basePath(path)).convert("RGBA")
+        return ctk.CTkImage(
+            light_image=img,
+            dark_image=img,
+            size=(largura, altura)
+        )
