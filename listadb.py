c0001280a2aa2416032dee5cb41b1571447eb60d
eliasasa <eliasnetoneto1@hotmail.com>
2024-10-01 20:10:33 -0400

    casinha da silva

--- listadb.py
+++ listadb.py
@@ -22,14 +22,24 @@ dic_teste = {}
 
 try:
     query = """
-    SELECT p.id_paciente, p.nome_paciente, s.temperatura, s.pressao_cardiaca, 
-           CONCAT(s.pressao_sistolica, '/', s.pressao_diastolica) as pressao_arterial, 
-           s.saturacao, p.status_paciente
-    FROM paciente p
-    JOIN sinal_vital s ON p.id_paciente = s.id_paciente
-    ORDER BY p.id_paciente  -- Garantir a ordem
+SELECT 
+    p.id_paciente, 
+    p.nome_paciente, 
+    s.temperatura, 
+    s.pressao_cardiaca, 
+    CONCAT(s.pressao_sistolica, '/', s.pressao_diastolica) AS pressao_arterial, 
+    s.saturacao, 
+    er.estado AS estado_risco,  -- Aqui estamos selecionando o estado
+    p.status_paciente
+FROM 
+    paciente p
+JOIN 
+    sinal_vital s ON p.id_paciente = s.id_paciente
+JOIN 
+    estado_risco er ON s.id_estado_risco = er.id_estado_risco  -- Certifique-se de que esta junção está correta
+ORDER BY 
+    p.id_paciente;
     """
-    
     db.exec(query)
     resultados = db.f_all()
     
@@ -39,7 +49,7 @@ try:
         dados_vitais = [row[2], row[3], row[4], row[5], row[6]]  # Temperatura, Pressão, Saturação, Status
         dic_teste[id_paciente] = {'nome': nome_paciente, 'dados': dados_vitais}
 
-    print(dic_teste)  # Para verificação dos pacientes
+    print(dic_teste)
 
 finally:
     db.close()
@@ -49,14 +59,13 @@ headers = ["Paciente", "Temperatura", "Cardíaca", "Arterial", "Saturação", "S
 search_frm = ctk.CTkFrame(janela, width=200, height=70)
 search_frm.place(relx=.05, rely=0.25)
 
-tt_lbl = ctk.CTkLabel(janela, text='Registros', font=(f.titulo_negrito), text_color='#1C3942')
+tt_lbl = ctk.CTkLabel(janela, text='Registros', font=(f.titulo_negrito), text_color='#48818D')
 tt_lbl.place(relx=.5, rely=0.18, anchor='center')
 
 # Frame principal
 reg_frm = ctk.CTkFrame(janela, width=1450, height=640, fg_color='transparent', corner_radius=10)
 reg_frm.place(relx=.2, rely=.23)
 
-# Configuração do grid para o cabeçalho
 reg_frm.grid_rowconfigure(0, weight=1)
 reg_frm.grid_rowconfigure(1, weight=99)
 reg_frm.columnconfigure(0, weight=1)
@@ -86,6 +95,22 @@ line_frame.rowconfigure(9, weight=1)
 x = 0
 linhas = []  # Lista para armazenar as linhas
 
+def hover_fake_enter(event, label):
+    label.configure(fg_color="light blue")
+    label.configure(cursor="hand2")
+
+def hover_fake_leave(event, label):
+    label.configure(fg_color="transparent")
+    label.configure(cursor='')
+
+# ID do label
+def search_id(event, nome_paciente, id_paciente, label):
+    print(f'O id do paciente {nome_paciente} é {id_paciente}!')
+    
+    label.configure(fg_color="#2D5E6C", text_color = 'white')
+
+    label.after(150, lambda: label.configure(fg_color="transparent", text_color='#1C3942'))
+    
 # Função para gerar as linhas na tabela
 def gerar_linhas(frame, headers, pacientes, start_index):
     max_char = 15
@@ -96,30 +121,39 @@ def gerar_linhas(frame, headers, pacientes, start_index):
 
         paciente_truncado = info['nome'] if len(info['nome']) <= max_char else info['nome'][:max_char] + "..."
 
-        # Dados paciente
-        linha_labels = []  # Lista para armazenar as labels da linha
+        linha_labels = []
         for idy, valor in enumerate([paciente_truncado] + info['dados']):
-            label = ctk.CTkLabel(linha_frame, text=valor, font=(f.simples), text_color='#1C3942')
+            label = ctk.CTkLabel(linha_frame, text=valor, font=(f.simples), text_color='#1C3942', corner_radius=10)
             if idy == 2:  # Add BPM no label
                 label.configure(text=f'{valor} BPM')
             elif idy == 5:  # Coluna 6 (status)
-                if info['dados'][-1] == 0:  # 0 sendo OK e 1 sendo RISCO
+                if info['dados'][-1] == 'Normal':  # 0 sendo OK e 1 sendo RISCO
                     label.configure(text='', image=img('imagens/check_icon.png', 30, 30))
                 else:
-                    label.configure(text='', image=img('imagens/alert_icon.png', 30, 26))
+                    label.configure(text='', image=img('imagens/alert_icon_red.png', 30, 30))
+
+            # Bind do search id
+            if idy == 0:
+                if idy == 0:
+                    label.bind("<Button-1>", lambda event, nome=info['nome'], id_paciente=id_paciente, lbl=label: search_id(event, nome, id_paciente, lbl))
+
+                # Hover fake
+                label.bind("<Enter>", lambda e, label=label: hover_fake_enter(e, label))
+                label.bind("<Leave>", lambda e, label=label: hover_fake_leave(e, label))
+
             label.grid(column=idy, row=0, sticky='news', ipady=3, padx=5, pady=5)
             label.grid_propagate(False)
-            linha_labels.append(label)  # Adiciona a label à lista
+            linha_labels.append(label)
 
         linha_frame.grid(row=idx, column=0, sticky='news', pady=0)
-        linhas.append((linha_frame, linha_labels, id_paciente))  # Adiciona o frame, as labels e o id à lista
+        linhas.append((linha_frame, linha_labels, id_paciente))
 
 gerar_linhas(line_frame, headers, dic_teste, x)
 
 # Funções para mudar de página
 def next_pag():
     global x
-    if (x)*10 < len(dic_teste) - 10:  # Limita o avanço de páginas
+    if (x)*10 < len(dic_teste) - 10:
         x += 1
         atualizar_linhas()
         pag_label.configure(text=f'{x+1}')
@@ -137,18 +171,31 @@ def back_pag():
 
 def atualizar_linhas():
     for idx, (linha_frame, linha_labels, id_paciente) in enumerate(linhas):
+        # Remove binds de clique e hover para não acumular 
+        linha_labels[0].unbind("<Button-1>")
+        linha_labels[0].unbind("<Enter>")
+        linha_labels[0].unbind("<Leave>") 
+
         if idx < 10:  # Limita até 10 linhas
             if (x * 10 + idx) < len(dic_teste):  # Verifica se existe paciente para essa linha
                 id_paciente = list(dic_teste.keys())[x * 10 + idx]
                 info = dic_teste[id_paciente]
+                
                 # Atualiza as labels com os novos dados
                 linha_labels[0].configure(text=info['nome'] if len(info['nome']) <= 15 else info['nome'][:15] + "...")
                 linha_labels[1].configure(text=info['dados'][0])  # Temperatura
                 linha_labels[2].configure(text=f"{info['dados'][1]} BPM")  # Cardíaca
                 linha_labels[3].configure(text=info['dados'][2])  # Arterial
                 linha_labels[4].configure(text=info['dados'][3])  # Saturação
-                # Atualiza status
-                linha_labels[5].configure(text='', image=img('imagens/check_icon.png', 30, 30) if info['dados'][-1] == 0 else img('imagens/alert_icon.png', 30, 26))
+                linha_labels[5].configure(
+                    text='',
+                    image=img('imagens/check_icon.png', 30, 30) if info['dados'][-1] == 'Normal' else img('imagens/alert_icon_red.png', 30, 30)
+                )
+
+                # Adiciona novamente os binds
+                linha_labels[0].bind("<Button-1>", lambda event, nome=info['nome'], id_paciente=id_paciente, lbl=linha_labels[0]: search_id(event, nome, id_paciente, lbl))
+                linha_labels[0].bind("<Enter>", lambda event, lbl=linha_labels[0]: hover_fake_enter(event, lbl))
+                linha_labels[0].bind("<Leave>", lambda event, lbl=linha_labels[0]: hover_fake_leave(event, lbl))
             else:
                 # Limpa as labels se não houver paciente
                 for label in linha_labels:
